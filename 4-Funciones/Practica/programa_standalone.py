6355f3906324a308f5e83923bc33aa8e54912845
Jon3J <jon181197@gmail.com>
2024-10-03 12:21:06 +0200

    cambios realizados

---
+++ 4-Funciones/Practica/programa_standalone.py
@@ -0,0 +1,12 @@
+import funciones
+
+print("Hola, me llamo Jon")
+lista = ["hola",55,True]
+for i in lista:
+    print(i)
+    
+funciones.func1()
+print(funciones.suma(6,7))
+print(funciones.resta(7,9))
+
+funciones.operaciones(100,90)
